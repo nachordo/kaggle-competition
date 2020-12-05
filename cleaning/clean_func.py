25123ead37dbb0a37d19370029cc128959935029
Nacho Ordovas <ordovaspascual@gmail.com>
2020-12-05 21:20:58 +0100

    Developed pipeline and different ways of treating cols

--- cleaning/clean_func.py
+++ cleaning/clean_func.py
@@ -14,6 +14,11 @@ import seaborn as sns
 def y_processing(y):
     return np.log10(y)
 
+
+cut={"Fair":1./5, "Good":2./5, "Very Good":3./5, "Premium":4./5, "Ideal":5./5}
+color={"D":1./7,"E":2./7,"F":3./7,"G":4./7,"H":5./7,"I":6./7,"J":7./7}
+clarity={"I1":1./8, "SI2":2./8, "SI1":3./8, "VS2":4./8, "VS1":5./8, "VVS2":6./8, "VVS1":7./8, "IF":8./8}
+
 def cleaning_1(diamonds):
     diamonds=diamonds.drop(columns=["depth","table"])
     
@@ -25,4 +30,71 @@ def cleaning_1(diamonds):
             col2 = dims[j]
             diamonds[f"{col1}*{col2}"] = diamonds[col1]*diamonds[col2]
     
+    return diamonds
+
+def cleaning_2(diamonds):
+    cut={"Fair":1./5, "Good":2./5, "Very Good":3./5, "Premium":4./5, "Ideal":5./5}
+    color={"D":1./7,"E":2./7,"F":3./7,"G":4./7,"H":5./7,"I":6./7,"J":7./7}
+    clarity={"I1":1./8, "SI2":2./8, "SI1":3./8, "VS2":4./8, "VS1":5./8, "VVS2":6./8, "VVS1":7./8, "IF":8./8}
+
+    diamonds=diamonds[["carat","cut","color","clarity"]]
+    diamonds["color"]=diamonds["color"].replace(color)
+    diamonds["cut"]=diamonds["cut"].replace(cut)
+    diamonds["clarity"]=diamonds["clarity"].replace(clarity)
+    
+    dims=["cut","color","clarity"]
+    for i in range(len(dims)):
+        for j in range(i,len(dims)):
+            col1 = dims[i]
+            col2 = dims[j]
+            diamonds[f"{col1}*{col2}"] = diamonds[col1]*diamonds[col2]
+    diamonds["all_cat"]=diamonds["color"]*diamonds["cut"]*diamonds["clarity"]
+    return diamonds
+
+def cleaning_3(diamonds):
+    diamonds=diamonds[["carat","cut","color","clarity"]]
+    diamonds = pd.get_dummies(diamonds,drop_first=True)
+    return diamonds    
+
+
+
+
+def cleaning_4(diamonds):
+    cut={"Fair":1./5, "Good":2./5, "Very Good":3./5, "Premium":4./5, "Ideal":5./5}
+    color={"D":1./7,"E":2./7,"F":3./7,"G":4./7,"H":5./7,"I":6./7,"J":7./7}
+    clarity={"I1":1./8, "SI2":2./8, "SI1":3./8, "VS2":4./8, "VS1":5./8, "VVS2":6./8, "VVS1":7./8, "IF":8./8}
+
+    diamonds=diamonds[["carat","cut","color","clarity"]]
+    diamonds["color"]=diamonds["color"].replace(color)
+    diamonds["cut"]=diamonds["cut"].replace(cut)
+    diamonds["clarity"]=diamonds["clarity"].replace(clarity)
+
+    diamonds["all_cat"]=diamonds["color"]*diamonds["cut"]*diamonds["clarity"]
+    return diamonds
+
+
+
+def cleaning_5(diamonds):
+    diamonds=diamonds[["x","z","cut","color","clarity"]]
+    diamonds = pd.get_dummies(diamonds,drop_first=True)
+    
+    return diamonds
+
+def cleaning_6(diamonds):
+    cut={"Fair":1./5, "Good":2./5, "Very Good":3./5, "Premium":4./5, "Ideal":5./5}
+    color={"D":1./7,"E":2./7,"F":3./7,"G":4./7,"H":5./7,"I":6./7,"J":7./7}
+    clarity={"I1":1./8, "SI2":2./8, "SI1":3./8, "VS2":4./8, "VS1":5./8, "VVS2":6./8, "VVS1":7./8, "IF":8./8}
+
+    diamonds=diamonds[["x","cut","color","clarity"]]
+    diamonds["color"]=diamonds["color"].replace(color)
+    diamonds["cut"]=diamonds["cut"].replace(cut)
+    diamonds["clarity"]=diamonds["clarity"].replace(clarity)
+    
+    dims=["cut","color","clarity"]
+    for i in range(len(dims)):
+        for j in range(i,len(dims)):
+            col1 = dims[i]
+            col2 = dims[j]
+            diamonds[f"{col1}*{col2}"] = diamonds[col1]*diamonds[col2]
+    diamonds["all_cat"]=diamonds["color"]*diamonds["cut"]*diamonds["clarity"]
     return diamonds
--- pipe/fitting_models.py
+++ pipe/fitting_models.py
@@ -10,20 +10,66 @@ import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn import linear_model
 from sklearn.metrics import r2_score,mean_squared_error
-
-
+from pipe.grid_searching import *
+from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import RandomForestRegressor
 
 from sklearn.model_selection import train_test_split
+
+
 def first_test(X,y):
-    X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8 )
-    
+    print("LinearRegression")
+    X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.85 )
     linreg = linear_model.LinearRegression()
     linreg.fit(X_train, y_train)
     y_pred = linreg.predict(X_train)
-    print("LinearRegression")
-    rmse = mean_squared_error(10**y_train.values, 10**y_pred, squared=False)
-    print("RMSE (train) =",rmse)
+    
     y_pred = linreg.predict(X_test)
     rmse = mean_squared_error(10**y_test.values, 10**y_pred, squared=False)
     print("RMSE (test) =",rmse)
-    return linreg
+    return linreg, rmse
+
+def forest_fit(X,y):
+    print("Forest")
+    X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8 )
+    func = random_tree_grid(X_train, y_train)
+    y_pred = func.predict(X_test)
+    rmse = mean_squared_error(10**y_test.values, 10**y_pred, squared=False)
+    print("RMSE (test) =",rmse)
+    return func, rmse
+
+def l2l1_fit(X,y):
+    print("L2 L1")
+    X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8 )
+    func = l2_l1_grid(X_train, y_train)
+    y_pred = func.predict(X_test)
+    rmse = mean_squared_error(10**y_test.values, 10**y_pred, squared=False)
+    print("RMSE (test) =",rmse)
+    return func, rmse
+
+def knn_fit(X,y):
+    print("KNN")
+    X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.8 )
+    func = knn_grid(X_train, y_train)
+    y_pred = func.predict(X_test)
+    rmse = mean_squared_error(10**y_test.values, 10**y_pred, squared=False)
+    print("RMSE (test) =",rmse)
+    return func, rmse
+
+def best_func(X,y):
+    best_one = None
+    best_rmse = 1_000_000
+    #lambda a,b: l2l1_fit(a,b),
+    f_list=[lambda a,b: first_test(a,b),
+            
+            lambda a,b: knn_fit(a,b),
+            lambda a,b: forest_fit(a,b) ]
+    
+    for func in f_list:
+        fnc,rmse = func(X,y)
+        if rmse < best_rmse:
+            print("Choosen one best",rmse, best_rmse,rmse < best_rmse)
+            best_rmse=rmse
+            best_one=fnc
+    
+    return best_one,best_rmse
